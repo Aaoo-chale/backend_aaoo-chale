d2909580de4805755aadd5c95f91d490a157ebcf
aaoo_chale <dbaaoochale@gmail.com>
2022-12-03 01:23:11 +0530

    add

--- route/vehicleRoute.js
+++ route/vehicleRoute.js
@@ -5,7 +5,7 @@ const vehicleController = require("../controller/vehicleController");
 const router = express.Router();
 
 router.get("/getAllCarsByUserId", vehicleController.getAllCarsByUserId);
-router.use(userAuthController.protect); //below this protected routes
+router.use(userAuthController.protect); //below this protected routes//
 router.post("/registerVehicle", vehicleController.registerVehicle);
 // router.get("/getAllCars", vehicleController.getAllCars);
 
