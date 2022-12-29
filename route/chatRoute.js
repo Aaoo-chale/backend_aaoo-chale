9f2d9d09a8996cccc18ac3bb45d5002622562691
aaoo_chale <dbaaoochale@gmail.com>
2022-12-30 00:28:50 +0530

    update

--- route/chatRoute.js
+++ route/chatRoute.js
@@ -5,10 +5,10 @@ const chatController = require("../controller/chatController");
 
 router.post("/createChat", chatController.createChat);
 router.post("/getAllChat", chatController.getAllChat);
-router.get("/getAllChatBySenderId", chatController.getAllChatBySenderId);
-router.get("/getAllChatByReceiverId", chatController.getAllChatByReceiverId);
-router.get("/getAllChatBySenderIdAndReceiverId", chatController.getAllChatBySenderIdAndReceiverId);
-router.use(userAuthController.protect);
+router.post("/getAllChatBySenderId", chatController.getAllChatBySenderId);
+router.post("/getAllChatByReceiverId", chatController.getAllChatByReceiverId);
+router.post("/getAllChatBySenderIdAndReceiverId", chatController.getAllChatBySenderIdAndReceiverId);
+// router.use(userAuthController.protect);
 // router.get("/getUserChatHistory", chatController.getUserChatHistory);
 
 module.exports = router;
